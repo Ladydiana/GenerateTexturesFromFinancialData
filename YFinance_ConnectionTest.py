c899311e443100dc35a62b9c3cc253f78e53b1b2
Ladydiana <diana.culincu@gmail.com>
2022-06-02 21:30:37 +0200

    yfinance test

---
+++ YFinance_ConnectionTest.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  2 19:46:30 2022
+
+@author: Ballerina
+"""
+
+#pip3 install yfinance
+
+import pandas as pd
+import yfinance as yf
+
+# Request historical data for past 5 years
+data = yf.Ticker("NVDA").history(period='5y')
+# Show info
+print(data.info())
